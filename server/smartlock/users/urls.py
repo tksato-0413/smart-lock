26ae494becdc98583c48df21ac49ca17c53842f1
tksato <ten.sato.0413@gmail.com>
2024-09-04 13:21:54 +0900

    ラズパイにHTTPアクセスを送るボタンを実装

--- server/smartlock/users/urls.py
+++ server/smartlock/users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('control-motor/', views.control_motor, name='control_motor'),
 ]
--- server/smartlock/users/views.py
+++ server/smartlock/users/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import CustomUserCreationForm
+from django.http import HttpResponse
+import requests
+from dotenv import load_dotenv
+import os
+
+
 
 def home(request):
     return render(request,'home.html')
+
+def control_motor(request):
+    if request.method == "POST":
+        try:
+            # .env ファイルの読み込み
+            load_dotenv()
+
+            # 環境変数からIPアドレスを取得
+            raspberry_pi_ip = os.getenv('')
+            url = f'http://{raspberry_pi_ip}:556/run_motor'  # Replace <raspberry_pi_ip> with your Raspberry Pi's IP
+            response = requests.post(url)
+            if response.status_code == 200:
+                return HttpResponse('Motor activated')
+            else:
+                return HttpResponse('Failed to activate motor')
+        except Exception as e:
+            return HttpResponse(f'Error: {str(e)}')
+    else:
+        return render(request, 'home.html')
 
 def register(request):
     if request.method == 'POST':
