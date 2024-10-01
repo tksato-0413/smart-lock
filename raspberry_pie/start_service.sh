6e276365353bcc94b2bfe0a6d9b8ee3f823fdb08
tksato <ten.sato.0413@gmail.com>
2024-10-01 15:23:23 +0900

    .gitignore　の修正

--- raspberry_pie/start_service.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Flask serverを起動
-/home/comnets/smart-lock/raspberry_pie/venv/bin/python /home/comnets/smart-lock/raspberry_pie/flask_server.py &
-
-# NFCリーダーを起動
-/home/comnets/smart-lock/raspberry_pie/venv/bin/python /home/comnets/smart-lock/raspberry_pie/nfc_reader/nfc_reader.py &
